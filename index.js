2cd8638fd33ea924464dc53817be0804e96f204c
Christopher Cohen <chris@cscohen.com>
2021-07-26 14:05:04 -0600

    commit tree 44082486cc50d4456699c623c3203ca806cea87d

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(bookNames => {
+    bookNames.forEach(book => {
+      const div = document.getElementById('main')
+      div.id = `book{book.id}`
+      const p = document.createElement('h2')
+      p.id = `book-title$ {book.id}`
+      p.textContent = `
+      Name: {book.name}
+      `  
+      //append list to dom adn create something to hold that data
+      div.append(p)
+      container.appendChild(div)
+
+      renderBooks()
+    })
+  })
+
 }
 
 function renderBooks(books) {
